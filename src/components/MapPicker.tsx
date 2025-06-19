47bf6fa80a354204ce589eb22611ab519d60f9d2
1luvin <luvindev404@gmail.com>
2025-06-20 01:09:39 +0200

    Quick fix

--- src/components/MapPicker.tsx
+++ src/components/MapPicker.tsx
@@ -5,7 +5,7 @@ import 'leaflet/dist/leaflet.css';
 import L from 'leaflet';
 import { useState } from 'react';
 
-delete (L.Icon.Default as any).prototype._getIconUrl;
+// delete (L.Icon.Default as any).prototype._getIconUrl;
 L.Icon.Default.mergeOptions({
   iconRetinaUrl: '/leaflet/marker-icon-2x.png',
   iconUrl: '/leaflet/marker-icon.png',
